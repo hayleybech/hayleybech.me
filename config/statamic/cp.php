c9975aa90bf2b2aacfffac43645df1171d190aa0
Hayley <hayleybech@gmail.com>
2024-12-06 21:14:22 +0800

    Downgrade statamic and laravel for php 8.1
    I did this by creating a fresh install then merging in the content

--- config/statamic/cp.php
+++ config/statamic/cp.php
@@ -17,21 +17,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication
-    |--------------------------------------------------------------------------
-    |
-    | Whether the Control Panel's authentication pages should be enabled,
-    | and where users should be redirected in order to authenticate.
-    |
-    */
-
-    'auth' => [
-        'enabled' => true,
-        'redirect_to' => null,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Start Page
     |--------------------------------------------------------------------------
     |
@@ -136,10 +121,6 @@ return [
     'custom_cms_name' => env('STATAMIC_CUSTOM_CMS_NAME', 'Statamic'),
 
     'custom_logo_url' => env('STATAMIC_CUSTOM_LOGO_URL', null),
-
-    'custom_dark_logo_url' => env('STATAMIC_CUSTOM_DARK_LOGO_URL', null),
-
-    'custom_logo_text' => env('STATAMIC_CUSTOM_LOGO_TEXT', null),
 
     'custom_favicon_url' => env('STATAMIC_CUSTOM_FAVICON_URL', null),
 
